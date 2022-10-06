017d552aafde451eeaf270f653ef780fa32397ef
klevessonroseno <klevesson.developer@gmail.com>
2022-10-06 09:31:42 -0300

    FindByEmail RepositoryCollaborators method

--- src/app/repository/CollaboratorRepository.js
+++ src/app/repository/CollaboratorRepository.js
@@ -2,6 +2,24 @@ import connect from '../../config/database';
 import { Collaborator } from '../domain/Collaborator';
 
 class CollaboratorRepository {
+  async findByEmail(email) {
+    const pgClient = await connect();
+    const sql = `
+      SELECT * FROM 
+        collaborators 
+      WHERE 
+        email = $1;
+    `;
+    const values = [ email ];
+    const result = await pgClient.query(sql, values);
+    const { rowCount, rows } = result;
+    
+    if(!rowCount) return null;
+
+    return rows;
+  }
+
+
   async store(name, email, password, userId) {
     const pgClient = await connect();
     const sql = `
@@ -12,9 +30,9 @@ class CollaboratorRepository {
         ($1, $2, $3, $4)
     `;
     const values = [ name, email, password, userId ];
-    const result = await pgClient.query(sql, values);
+    const { rowCount } = await pgClient.query(sql, values);
 
-    return result;
+    return rowCount;
   }
 }
 
