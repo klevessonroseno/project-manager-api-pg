6b722ba838e747a7f36aa7db7ae50efa9a99ae1f
klevessonroseno <klevesson.developer@gmail.com>
2022-10-06 09:16:33 -0300

    Collaborators store route

--- src/routes/private/routes.js
+++ src/routes/private/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import usersResources from '../../app/resources/UsersResources';
+import collaboratorsResources from '../../app/resources/CollaboratorsResources';
 import authMiddleware from '../../app/middlewares/auth';
 
 const routes = new express.Router();
@@ -7,5 +8,7 @@ const routes = new express.Router();
 routes.use(authMiddleware);
 
 routes.put('/users', usersResources.update);
+
+routes.post('/users/collaborators', collaboratorsResources.store);
 
 export default routes;
