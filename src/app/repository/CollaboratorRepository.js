4c90af6a5e87eaff9592932455916b7c02cb8fd1
klevessonroseno <klevesson.developer@gmail.com>
2022-10-06 09:09:39 -0300

    Fixing method to save collaborator.

--- src/app/repository/CollaboratorRepository.js
+++ src/app/repository/CollaboratorRepository.js
@@ -2,16 +2,16 @@ import connect from '../../config/database';
 import { Collaborator } from '../domain/Collaborator';
 
 class CollaboratorRepository {
-  async store(name, email, password) {
+  async store(name, email, password, userId) {
     const pgClient = await connect();
     const sql = `
       INSERT INTO 
         collaborators 
-        (name, email, password) 
+        (name, email, password, user_id) 
       VALUES 
-        ($1, $2, $3)
+        ($1, $2, $3, $4)
     `;
-    const values = [ name, email, password ];
+    const values = [ name, email, password, userId ];
     const result = await pgClient.query(sql, values);
 
     return result;
