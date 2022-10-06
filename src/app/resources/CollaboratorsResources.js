03ee0d53e6f0f6f0d7d6dcc7c8765920753664b9
klevessonroseno <klevesson.developer@gmail.com>
2022-10-06 09:15:00 -0300

    Collaborators store

--- src/app/resources/CollaboratorsResources.js
+++ src/app/resources/CollaboratorsResources.js
@@ -1,7 +1,14 @@
 import collaboratorRepository from '../repository/CollaboratorRepository';
 
 class CollaboratorsResources {
+  async store(request, response) {
+    const { userId } = request;
+    const { name, email, password } = request.body;
 
+    const result = await collaboratorRepository.store(name, email, password, userId);
+
+    return response.json({ result });
+  }
 }
 
 export default new CollaboratorsResources();
