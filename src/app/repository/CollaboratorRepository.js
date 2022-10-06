158f89c9248b735460c428f12ee8b90c58e2bf95
klevessonroseno <klevesson.developer@gmail.com>
2022-10-06 09:07:53 -0300

    Creating method to save collaborator.

--- src/app/repository/CollaboratorRepository.js
+++ src/app/repository/CollaboratorRepository.js
@@ -2,7 +2,20 @@ import connect from '../../config/database';
 import { Collaborator } from '../domain/Collaborator';
 
 class CollaboratorRepository {
-  
+  async store(name, email, password) {
+    const pgClient = await connect();
+    const sql = `
+      INSERT INTO 
+        collaborators 
+        (name, email, password) 
+      VALUES 
+        ($1, $2, $3)
+    `;
+    const values = [ name, email, password ];
+    const result = await pgClient.query(sql, values);
+
+    return result;
+  }
 }
 
 export default new CollaboratorRepository();
