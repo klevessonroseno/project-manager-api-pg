ba8327f4f7b1766cec4cb82706c256b60f4be5d1
klevessonroseno <klevesson.developer@gmail.com>
2022-10-08 10:43:46 -0300

    Add getters and setters to Collaborators class

--- src/app/domain/Collaborator.ts
+++ src/app/domain/Collaborator.ts
@@ -1,15 +1,54 @@
+import { v4 as uuidv4 } from "uuid";
+
 export class Collaborator {
-  constructor(
-    public id: number, 
-    public name: string, 
-    public email: string, 
-    public password: string, 
-    public userId: string
-    ) {
-    this.id = id;
+  private readonly id: string; 
+  private name: string; 
+  private email: string; 
+  private password: string; 
+  private readonly userId: string;
+
+  constructor( 
+    name: string, 
+    email: string, 
+    password: string, 
+    userId: string,
+  ) {
+    this.id = uuidv4();
     this.name = name;
     this.email = email;
     this.password = password;
     this.userId = userId;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(email: string): void {
+    this.email = email;
+  }
+
+  public getPassword(): string {
+    return this.password;
+  }
+
+  public setPassword(password: string): void {
+    this.password = password;
+  }
+
+  public getUserId(): string {
+    return this.userId;
   }
 }
