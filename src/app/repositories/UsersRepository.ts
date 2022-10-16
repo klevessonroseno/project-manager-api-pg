517af87c4b3308f0574c076507ec0e59adb7f4de
klevessonroseno <klevesson.developer@gmail.com>
2022-10-16 10:21:26 -0300

    Refactoring UsersRepository.

--- src/app/repositories/UsersRepository.ts
+++ src/app/repositories/UsersRepository.ts
@@ -5,11 +5,10 @@ class UsersRepository {
   async save(id: string, name: string, email: string, password: string): Promise<boolean> { 
     const client = await pool.connect();
     const sql = `         
-      INSERT INTO
-        users 
-          (id, name, email, password)
-        VALUES
-          ($1, $2, $3, $4)
+      INSERT INTO users 
+        (id, name, email, password)
+      VALUES
+        ($1, $2, $3, $4)
     `;
     const values = [ id, name, email, password ];
     const { rowCount } = await client.query(sql, values);
@@ -22,7 +21,6 @@ class UsersRepository {
   }
 
   async update(user: User): Promise<boolean> {
-
     const id = user.getId();
     const name = user.getName();
     const email = user.getEmail();
@@ -32,17 +30,12 @@ class UsersRepository {
     const values = [ name, email, password, id ];
 
     const sql = `
-      UPDATE
-        users
-      SET
-        name = $1,
-        email = $2,
-        password = $3
-      WHERE 
-        id LIKE $4
+      UPDATE users 
+      SET name = $1, email = $2, password = $3 
+      WHERE id LIKE $4
     `;
     const { rowCount } = await client.query(sql, values);
-
+    
     client.release();
 
     if(rowCount && rowCount !== 0) return true;
@@ -52,16 +45,7 @@ class UsersRepository {
 
   async checkIfUserExistsByEmail(email: string): Promise<boolean> {
     const client = await pool.connect();
-    const sql = `
-      SELECT 
-        user_email 
-      FROM 
-        users
-      WHERE
-        user_email 
-      LIKE 
-        $1
-    `;
+    const sql = `SELECT email FROM users WHERE email LIKE $1`;
     const values = [ email ];
     const { rowCount } = await client.query(sql, values);
     
@@ -69,24 +53,25 @@ class UsersRepository {
 
     if(rowCount && rowCount !== 0) return true;
 
-    return false; 
+    return false;
+  }
+
+  async checkIfUserExistsById(id: string): Promise<boolean> {
+    const client = await pool.connect();
+    const sql = `SELECT id FROM users WHERE id LIKE $1`;
+    const values = [ id ];
+    const { rowCount } = await client.query(sql, values);
+    
+    client.release();
+
+    if(rowCount && rowCount !== 0) return true;
+
+    return false;
   }
 
   async findByEmail(userEmail: string): Promise<User> {
     const client = await pool.connect();
-    const sql = `
-      SELECT
-        user_id "id",
-        user_name "name",
-        user_email "email",
-        user_password "password"
-      FROM 
-        users
-      WHERE
-        user_email 
-      LIKE 
-        $1
-    `;
+    const sql = `SELECT * FROM users WHERE email LIKE $1`;
     const values = [ userEmail ];
     const { rows } = await client.query(sql, values);
     const [{ id, name, email, password }] = rows;
@@ -96,19 +81,7 @@ class UsersRepository {
 
   async findById(userId: string): Promise<User> {
     const client = await pool.connect();
-    const sql = `
-      SELECT
-        id "id",
-        name "name",
-        email "email",
-        password "password"
-      FROM 
-        users
-      WHERE
-        id 
-      LIKE 
-        $1
-    `;
+    const sql = `SELECT * FROM users WHERE id LIKE $1`;
     const values = [ userId ];
     const { rows } = await client.query(sql, values);
 
